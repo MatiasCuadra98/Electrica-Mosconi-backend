0335e31ab15e08fa0db3af02aed369c8aaac4f3f
Virginia Montoya <mvirginiamontoya@yahoo.com.ar>
2024-06-27 16:00:12 -0300

    reordeno modelo msg sent

--- src/models/MsgSent.js
+++ src/models/MsgSent.js
@@ -12,46 +12,46 @@ module.exports = (sequelize) => {
       type: DataTypes.TEXT,
       allowNull: false
     },
-        toData: {
-            type: DataTypes.JSON,
-            allowNull: false,
-            defaultValue: {}, // Default value for the object if not specified
-            validate: {
-              isObject(value) {
-                if (typeof value !== 'object') {
-                  throw new Error('yourObjectName must be an object');
-                }
-              },
-              hasAppAndValue(value) {
-                if (!value.app || !value.value) {
-                  throw new Error('yourObjectName must have both app and value properties');
-                }
-              },
+    toData: {
+      type: DataTypes.JSON,
+      allowNull: false,
+      defaultValue: {},
+      validate: {
+        isObject(value) {
+          if (typeof value !== 'object') {
+            throw new Error('toData must be an object');
+          }
+        },
+        hasAppAndValue(value) {
+          if (!value.app || !value.value) {
+            throw new Error('toData must have both app and value properties');
+          }
+        },
+      },
+    },
+    message: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    chatId: {
+      type: DataTypes.BIGINT,
+      allowNull: false
+    },
+    timestamp: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW
+    },
+    BusinessId: {
+      type: DataTypes.UUID,
+      allowNull: true, // Permite valores nulos
+    },
+            //ESTO ES NUEVO:
+            received: {
+              type: DataTypes.BOOLEAN,
+              allowNull: false,
+              defaultValue: false
             },
-          },
-        message:{
-            type:DataTypes.STRING,
-            allowNull:false
-        },
-        chatId: {
-          type: DataTypes.BIGINT,
-          allowNull: false
-        },
-        timestamp: {
-          type: DataTypes.DATE,
-          allowNull: false,
-          defaultValue: DataTypes.NOW
-        },
-        BusinessId: {
-          type: DataTypes.UUID,
-          allowNull: true, // Permite valores nulos
-        },
-        //ESTO ES NUEVO:
-        received: {
-          type: DataTypes.BOOLEAN,
-          allowNull: false,
-          defaultValue: false
-        },
   }, { timestamps: false });
 
 };
