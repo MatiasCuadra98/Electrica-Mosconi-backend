3e6468894b955531e9a742ae5b6f5db70db08820
Virginia Montoya <mvirginiamontoya@yahoo.com.ar>
2024-06-27 19:48:43 -0300

    agrego atributos a business

--- src/controllers/Business/businessPost.js
+++ src/controllers/Business/businessPost.js
@@ -1,12 +1,12 @@
 const createBusiness = require('../../handlers/Business/createBusiness')
 
 const handlerBusinessPost = async(req,res) =>{
-    const {name,phone, email,apiKey,srcName} = req.body
+    const {name,password, address, city, country, phone, email,apiKey,srcName} = req.body
     console.log(name,phone, email,apiKey,srcName);
     try {
-        if(!name || !phone || !email || !apiKey || !srcName) return res.status(404).json({error:'required data not found'})
+        if(!name || !password || !country || !phone || !email || !apiKey || !srcName) return res.status(404).json({error:'required data not found'})
         // const newBusiness = await createBusiness(name,phone,email,SuperuserId)
-        const newBusiness = await createBusiness({name,phone,email,apiKey,srcName})
+        const newBusiness = await createBusiness({name,password, address, city, country, phone, email,apiKey,srcName})
         if(!newBusiness) return res.status(404).json({error:'Business not found'})
         return res.status(200).json(newBusiness)
     } catch (error) {
