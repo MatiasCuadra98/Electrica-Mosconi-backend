37fd58308f27a2f5b2c6fa4cee07ed54d7c1cc2e
Matías Cuadra <MatiasCuadra98>
2024-08-09 16:25:01 -0300

    modifico ruta de whatsapp

--- src/routes/whatsapp/wspMessageWebhook.js
+++ src/routes/whatsapp/wspMessageWebhook.js
@@ -2,6 +2,21 @@ const { Router } = require('express');
 const { handleMessage } = require('../../whatsappApi/whatsapp');
 const wspMessageWebhook = Router();
 
+
+wspMessageWebhook.get('/whatsapp/wspMessageWebhook', (req, res) => {
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+
+  // Verifica el token de verificación
+  if (mode === 'subscribe' && token === process.env.WEBHOOK_VERIFY_TOKEN) {
+    res.status(200).send(challenge);
+    console.log('Webhook verified successfully!');
+  } else {
+    res.sendStatus(403);
+  }
+});
+
 wspMessageWebhook.post('/whatsapp/wspMessageWebhook', async (req, res) => {
   const { entry } = req.body;
 
@@ -19,5 +34,6 @@ wspMessageWebhook.post('/whatsapp/wspMessageWebhook', async (req, res) => {
     res.status(500).json({ success: false, message: 'Error procesando el mensaje de whatsapp' });
   }
 });
+
 
 module.exports = wspMessageWebhook;
