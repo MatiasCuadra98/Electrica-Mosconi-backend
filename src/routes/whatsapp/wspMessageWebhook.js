5065e0ec9e5b4922c87ddcc09b5c5eb9bdce4746
Matías Cuadra <MatiasCuadra98>
2024-08-12 15:20:56 -0300

    modifico ruta de recepcion de mensajes

--- src/routes/whatsapp/wspMessageWebhook.js
+++ src/routes/whatsapp/wspMessageWebhook.js
@@ -2,39 +2,38 @@ const { Router } = require('express');
 const { handleMessage } = require('../../whatsappApi/whatsapp');
 const wspMessageWebhook = Router();
 
-wspMessageWebhook.all('/whatsapp', async (req, res) => {
-  if (req.method === 'GET') {
-    // Verificación del webhook
-    const mode = req.query['hub.mode'];
-    const token = req.query['hub.verify_token'];
-    const challenge = req.query['hub.challenge'];
 
-    if (mode === 'subscribe' && token === process.env.WEBHOOK_VERIFY_TOKEN) {
-      res.status(200).send(challenge);
-      console.log('Webhook verified successfully!');
-    } else {
-      res.sendStatus(403);
-    }
-  } else if (req.method === 'POST') {
-    // Recepción de mensajes
-    const { entry } = req.body;
+wspMessageWebhook.get('/whatsappGet', (req, res) => {
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
 
-    try {
-      for (let change of entry[0].changes) {
-        const message = change.value.messages[0];
-        if (message) {
-          console.log('Procesando mensaje de WhatsApp:', message);
-          await handleMessage(message);
-        }
-      }
-      res.status(200).end();
-    } catch (error) {
-      console.error('Error procesando el mensaje de WhatsApp:', error);
-      res.status(500).json({ success: false, message: 'Error procesando el mensaje de WhatsApp' });
-    }
+  // Verifica el token de verificación
+  if (mode === 'subscribe' && token === process.env.WEBHOOK_VERIFY_TOKEN) {
+    res.status(200).send(challenge);
+    console.log('Webhook verified successfully!');
   } else {
-    res.sendStatus(405); // Método no permitido
+    res.sendStatus(403);
   }
 });
+
+wspMessageWebhook.post('/wspMessageWebhook', async (req, res) => {
+  const { entry } = req.body;
+
+  try {
+    for (let change of entry[0].changes) {
+      const message = change.value.messages[0];
+      if (message) {
+        console.log('Procesando mensaje de whatsapp:', message)
+        await handleMessage(message);
+      }
+    }
+    res.status(200).end();
+  } catch (error) {
+    console.error('Error procesando el mensaje de whatsapp:', error);
+    res.status(500).json({ success: false, message: 'Error procesando el mensaje de whatsapp' });
+  }
+});
+
 
 module.exports = wspMessageWebhook;
