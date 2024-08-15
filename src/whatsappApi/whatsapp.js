43700c4945cd86b5d8766b77d932b798adac60c1
Matías Cuadra <MatiasCuadra98>
2024-08-15 11:20:18 -0300

    saco el id de la constante en la linea 9 y lo coloco directamente en la url a ver que devuelve el log

--- src/whatsappApi/whatsapp.js
+++ src/whatsappApi/whatsapp.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const businessId = "dcb75f4c-5c96-40c5-befc-3179c96535c2"; 
 const socialMediaId = 2; // Este es el id de WhatsApp en SocialMedia
 const GRAPH_API_TOKEN = process.env.GRAPH_API_TOKEN; // Usa tu token de WhatsApp Business API
-const BUSINESS_PHONE_NUMBER_ID  = process.env.BUSINESS_PHONE_NUMBER_ID || 372206589314811;
+const BUSINESS_PHONE_NUMBER_ID  = process.env.BUSINESS_PHONE_NUMBER_ID ;
 
 
 const handleMessage = async (msg) => {
@@ -85,7 +85,7 @@ const sendMessage = async (to, message) => {
   console.log("Business phone id :", BUSINESS_PHONE_NUMBER_ID );
 
   try {
-    const response = await axios.post(`https://graph.facebook.com/v20.0/${BUSINESS_PHONE_NUMBER_ID}/messages`, {
+    const response = await axios.post(`https://graph.facebook.com/v20.0/372206589314811/messages`, {
       messaging_product: "whatsapp",
       to: to,
       text: { body: message }
