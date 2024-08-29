04a8cb9a977f0d4d5bc3b6df7717d09f6967eb40
Virginia Montoya <mvirginiamontoya@yahoo.com.ar>
2024-08-29 17:10:32 -0300

    agrego telegram a la ruta de messageWebhook

--- src/routes/telegram/messageWebhook.js
+++ src/routes/telegram/messageWebhook.js
@@ -5,7 +5,7 @@ const {Business,User,MsgReceived, Contacts, SocialMedia} = require('../../db')
 
 module.exports = (io)=>{
     //ruta para recibir mensajes
-    messageWebhook.post('/messageWebHook', async (req, res) =>{
+    messageWebhook.post('/telegram/messageWebHook', async (req, res) =>{
       console.log('mensaje recibido:', req.body);
       const businessId = "5e31d0fb-87b5-4ccf-b150-e730872c7a0e"; 
       const socialMediaId = 1; //id de telegram
