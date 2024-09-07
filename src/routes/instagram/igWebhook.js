7ee4ee5ae4b8ef03bd6e3c8352068365d58bc409
Matías Cuadra <MatiasCuadra98>
2024-09-07 18:03:08 -0300

    modifico rutas de llegada de msj de ig

--- src/routes/instagram/igWebhook.js
+++ src/routes/instagram/igWebhook.js
@@ -3,13 +3,15 @@ const igWebhook = Router();
 
 //webhook para recibir mensajes y procesarlos
 igWebhook.post('/webhook/instagram', (req, res) => {
+  console.log('Solicitud recibida en /webhook/instagram:', req.body);
+
   const body = req.body;
   
   if (body.object === 'instagram') {
-    // Process the Instagram direct message event
+    // Procesamos el mensaje directo de ig
     body.entry.forEach(entry => {
       const messageEvent = entry.messaging[0];
-      // Log or save the message to the database
+      // Log para ver si llega el mensaje, aca iria la logica para guardar el msj en la bd
       console.log("Mensaje recibido: ", messageEvent);
     });
     res.status(200).send('EVENT_RECEIVED');
@@ -19,21 +21,19 @@ igWebhook.post('/webhook/instagram', (req, res) => {
 });
 
 //Verificacion del token
-igWebhook.get('/webhook/instagram', (req, res) => {
-  const VERIFY_TOKEN = 'emigverifytoken'; // token de verificacion, tiene que ser el mismo que coloquemos en meta
-
-  const mode = req.query['hub.mode'];
-  const token = req.query['hub.verify_token'];
-  const challenge = req.query['hub.challenge'];
-
-  if (mode && token) {
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      console.log('Webhook verificado.');
-      res.status(200).send(challenge);
-    } else {
-      res.sendStatus(403);
-    }
-  }
-});
+ igWebhook.get('/webhook/instagram', (req, res) => {
+   const VERIFY_TOKEN = 'emigverifytoken'; // token de verificacion, tiene que ser el mismo que coloquemos en meta
+   const mode = req.query['hub.mode'];
+   const token = req.query['hub.verify_token'];
+   const challenge = req.query['hub.challenge'];
+   if (mode && token) {
+     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+       console.log('Webhook verificado.');
+       res.status(200).send(challenge);
+     } else {
+       res.sendStatus(403);
+     }
+   }
+ });
 
 module.exports = igWebhook
