6a5d56942d3b20b0da5d7c140b6b18708e55d0e8
Matías Cuadra <MatiasCuadra98>
2024-09-09 14:28:48 -0300

    agrego check de firma de meta

--- src/routes/instagram/igWebhook.js
+++ src/routes/instagram/igWebhook.js
@@ -1,17 +1,32 @@
 const { Router } = require('express');
 const igWebhook = Router();
+const crypto = require('crypto');
 
-//webhook para recibir mensajes y procesarlos
+//  clave secreta de app de Meta
+const appSecret = process.env.APP_SECRET || 'c9055ea3d6f4d713da39caeb76cb6929';
+
+// Middleware para validar la firma de la solicitud
 igWebhook.post('/webhook/instagram', (req, res) => {
+  const signature = req.headers['x-hub-signature-256'];
+  const payload = JSON.stringify(req.body);
+
+  // Calcula la firma esperada usando la clave secreta
+  const expectedSignature = `sha256=${crypto.createHmac('sha256', appSecret).update(payload).digest('hex')}`;
+
+  if (signature !== expectedSignature) {
+    console.error("Firma no válida");
+    return res.sendStatus(403); 
+  }
+
   console.log('Solicitud recibida en /webhook/instagram:', req.body);
 
   const body = req.body;
   
   if (body.object === 'instagram') {
-    // Procesamos el mensaje directo de ig
+    // Procesamos el mensaje directo de IG
     body.entry.forEach(entry => {
       const messageEvent = entry.messaging[0];
-      // Log para ver si llega el mensaje, aca iria la logica para guardar el msj en la bd
+      // Log para ver si llega el mensaje, aquí iría la lógica para guardar el mensaje en la BD
       console.log("Mensaje recibido: ", messageEvent);
     });
     res.status(200).send('EVENT_RECEIVED');
@@ -20,20 +35,21 @@ igWebhook.post('/webhook/instagram', (req, res) => {
   }
 });
 
-//Verificacion del token
- igWebhook.get('/webhook/instagram', (req, res) => {
-   const VERIFY_TOKEN = 'emigverifytoken'; // token de verificacion, tiene que ser el mismo que coloquemos en meta
-   const mode = req.query['hub.mode'];
-   const token = req.query['hub.verify_token'];
-   const challenge = req.query['hub.challenge'];
-   if (mode && token) {
-     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-       console.log('Webhook verificado.');
-       res.status(200).send(challenge);
-     } else {
-       res.sendStatus(403);
-     }
-   }
- });
+// Verificación del token
+igWebhook.get('/webhook/instagram', (req, res) => {
+  const VERIFY_TOKEN = 'emigverifytoken'; // Token de verificación, tiene que ser el mismo que coloquemos en Meta
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+  
+  if (mode && token) {
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      console.log('Webhook verificado.');
+      res.status(200).send(challenge);
+    } else {
+      res.sendStatus(403);
+    }
+  }
+});
 
-module.exports = igWebhook
+module.exports = igWebhook;
