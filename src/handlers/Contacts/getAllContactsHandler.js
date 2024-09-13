fcee38e37da14038b72271c38a5207f394fc9116
Virginia Montoya <mvirginiamontoya@yahoo.com.ar>
2024-09-13 16:29:51 -0300

    cambio controller del search

--- src/handlers/Contacts/getAllContactsHandler.js
+++ src/handlers/Contacts/getAllContactsHandler.js
@@ -2,11 +2,11 @@ const {getAllContacts} = require("../../controllers/Contacts/getAllContacts")
 const {searchContact} = require("../../controllers/Contacts/searchContact")
 const getAllContactsHandler = async(req, res) => {
     const {name, phone, userName, Email} = req.query;
-    console.log('name: ', name, 'phone: ', phone, 'userName: ', userName, 'Email: ', Email);
+    console.log('llega al handler esta data => name: ', name, 'phone: ', phone, 'userName: ', userName, 'Email: ', Email);
     
      try {
         const search = name || phone || userName || Email;
-        console.log('search', search);
+        console.log('search en try del handler: ', search);
         
     const allContacts = search ? await searchContact(search): await getAllContacts()
     !allContacts.length ? res.status(400).send('Contacts not found') : res.status(200).json(allContacts)
