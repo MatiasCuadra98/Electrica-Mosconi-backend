dacf93e5dc641839c2c9e774c56c69b47f161436
Virginia Montoya <mvirginiamontoya@yahoo.com.ar>
2024-09-13 13:25:57 -0300

    agrego getAllContacts completo + search

---
+++ src/handlers/Contacts/getAllContactsHandler.js
@@ -0,0 +1,15 @@
+const {getAllContacts} = require("../../controllers/Contacts/getAllContacts")
+const {searchContact} = require("../../controllers/Contacts/searchContact")
+const getAllContactsHandler = async(req, res) => {
+    const {name, phone, userName, Email} = req.query;
+     try {
+        const search = name || phone || userName || Email
+    const allContacts = search ? await searchContact(search): await getAllContacts()
+    !allContacts.length ? res.status(400).send('Contacts not found') : res.status(200).json(allContacts)
+ } catch (error) {
+    res.status(500).json({error: error.message})
+ }
+}
+
+module.exports = {getAllContactsHandler}
+
--- src/routes/contactRoutes.js
+++ src/routes/contactRoutes.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
+const {getAllContactsHandler} = require("../handlers/Contacts/getAllContactsHandler")
 const {
   getContactByIdHandler,
 } = require("../handlers/Contacts/getContactByIdHandler");
 
 const contactRoute = Router();
 
+contactRoute.get("/", getAllContactsHandler)
 contactRoute.get("/:id", getContactByIdHandler);
 
 module.exports = contactRoute;
