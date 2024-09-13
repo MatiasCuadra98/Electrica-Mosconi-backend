c55afa03401d740abb2176a61b34c1de2a080aba
Virginia Montoya <mvirginiamontoya@yahoo.com.ar>
2024-09-13 18:39:52 -0300

    agrego el filtro de comparar si el value es null

--- src/controllers/Contacts/searchContact.js
+++ src/controllers/Contacts/searchContact.js
@@ -5,24 +5,16 @@ const {Op, Sequelize} = require('sequelize');
 const searchContact = async(search) => {
 
     console.log('search por query:', search);
-    console.log('search tipo de dato', typeof(search));
+   // console.log('search tipo de dato', typeof(search));
 
     const contactsFiltered = await  Contacts.findAll({
         where: {
-            // [Op.or]: [
-            //     { name: { [Op.iLike]: `%${search}%` } },
-            //     { userName: { [Op.iLike]: `%${search}%` } },
-            //     //{ phone: { [Op.iLike]: `%${search}%` } }, // Se trata como número, convertirlo a string
-            //     { Email: { [Op.iLike]: `%${search}%` } },
-            //     { phone: Sequelize.cast(Sequelize.col('phone'), 'TEXT'), [Op.iLike]: `%${search}%`} // Convierte BIGINT a TEXT para permitir iLike
-            // ]
             [Op.or]: [
                 {
                   name: { [Op.iLike]: `%${search}%`},
-                  userName: {[Op.iLike]: `%${search}%`},
-                  //phone: {[Op.iLike]: `%${search}%`},
-                  Email: {[Op.iLike]: `%${search}%`},
-                  //phone: {Sequelize.cast(Sequelize.col('phone'), 'TEXT'), [Op.iLike]: `%${search}%`} 
+                  userName: {[Op.iLike]: `%${search}%`, [Op.not]: null},
+                  //phone: {[Op.iLike]: `%${search}%`, [Op.not]: null},
+                  Email: {[Op.iLike]: `%${search}%`, [Op.not]: null},
                 },
             ],
         }
