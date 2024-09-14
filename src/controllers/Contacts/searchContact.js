6c15167e31bb295b02e47dfa5d9d0ebfb11172ac
Virginia Montoya <mvirginiamontoya@yahoo.com.ar>
2024-09-14 01:14:35 -0300

    acomodo el codigo y quito logs

--- src/controllers/Contacts/searchContact.js
+++ src/controllers/Contacts/searchContact.js
@@ -1,19 +1,14 @@
-const {getAllContacts} = require("./getAllContacts")
 const {Contacts} = require("../../db");
-const {Op, Sequelize} = require('sequelize');
+const {Op} = require('sequelize');
 
 const searchContact = async(search) => {
-
-    console.log('search por query:', search);
-   // console.log('search tipo de dato', typeof(search));
-
     const contactsFiltered = await  Contacts.findAll({
-        where: {
-            name: { [Op.iLike]: `%${search}%`},    
-        }
+        where: {name: { [Op.iLike]: `%${search}%`}}
     })
+    
     if(!contactsFiltered.length) throw new Error('There are not contacts that match the search')
-        return contactsFiltered
+    
+    return contactsFiltered
 }
 
 module.exports = {searchContact}
--- src/handlers/Contacts/getAllContactsHandler.js
+++ src/handlers/Contacts/getAllContactsHandler.js
@@ -2,16 +2,15 @@ const {getAllContacts} = require("../../controllers/Contacts/getAllContacts")
 const {searchContact} = require("../../controllers/Contacts/searchContact")
 
 const getAllContactsHandler = async(req, res) => {
-    const {name } = req.query;
-    console.log('llega al handler esta data => name: ', name);
-    
-     try {
-        const search = name;
-        console.log('search en try del handler: ', search);
-        
-    const allContacts = search ? await searchContact(search): await getAllContacts()
-    !allContacts.length ? res.status(400).send('Contacts not found') : res.status(200).json(allContacts)
- } catch (error) {
+   const {name } = req.query;
+   
+   try {
+      const search = name;
+      const allContacts = search ? await searchContact(search): await getAllContacts()
+      
+      !allContacts.length ? res.status(400).send('Contacts not found') : res.status(200).json(allContacts)
+   
+   } catch (error) {
     res.status(500).json({error: error.message})
  }
 }
