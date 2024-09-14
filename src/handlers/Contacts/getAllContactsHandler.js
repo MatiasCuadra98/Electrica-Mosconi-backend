1d43bcfd1cd503e27fbd91e88d5fe3cf5d91af9f
Virginia Montoya <mvirginiamontoya@yahoo.com.ar>
2024-09-14 00:17:16 -0300

    quito phone de handler

--- src/handlers/Contacts/getAllContactsHandler.js
+++ src/handlers/Contacts/getAllContactsHandler.js
@@ -1,11 +1,12 @@
 const {getAllContacts} = require("../../controllers/Contacts/getAllContacts")
 const {searchContact} = require("../../controllers/Contacts/searchContact")
+
 const getAllContactsHandler = async(req, res) => {
-    const {name, phone, userName, Email} = req.query;
-    console.log('llega al handler esta data => name: ', name, 'phone: ', phone, 'userName: ', userName, 'Email: ', Email);
+    const {name, userName, Email} = req.query;
+    console.log('llega al handler esta data => name: ', name, 'userName: ', userName, 'Email: ', Email);
     
      try {
-        const search = name || phone || userName || Email;
+        const search = name || userName || Email;
         console.log('search en try del handler: ', search);
         
     const allContacts = search ? await searchContact(search): await getAllContacts()
