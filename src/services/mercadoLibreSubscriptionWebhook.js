7d3da551c90c2dc477e9473f07af921c68f89e38
Matías Cuadra <MatiasCuadra98>
2024-09-20 18:34:13 -0300

    modifico url de suscripcion al webhook

--- src/services/mercadoLibreSubscriptionWebhook.js
+++ src/services/mercadoLibreSubscriptionWebhook.js
@@ -1,19 +1,16 @@
 const axios = require("axios");
 
 async function subscribeToWebhook() {
-  const APP_ID = "3149999715702183"; //id de meli
-  const ACCESS_TOKEN =
-    "APP_USR-3149999715702183-092016-7c6043e53856c25b46984ac65c38f5bf-232533265"; // cuando te autenticas te dan el token
-  const CALLBACK_URL =
-    "https://electrica-mosconi-server.onrender.com/webhook/mercadolibre"; // esta seria la url donde recibimos notificaciones
+  const ACCESS_TOKEN = "APP_USR-3149999715702183-092016-7c6043e53856c25b46984ac65c38f5bf-232533265"; 
+  const CALLBACK_URL = "https://electrica-mosconi-server.onrender.com/webhook/mercadolibre";
+  const USER_ID = "232533265"; // ID del usuario autenticado
 
   try {
-    // URL para suscribirme a los webhooks en Mercado Libre
+    // URL correcta para suscribirte a los webhooks
     const response = await axios.post(
-      `https://api.mercadolibre.com/applications/${APP_ID}/notifications`,
+      `https://api.mercadolibre.com/users/${USER_ID}/subscriptions`,
       {
-        user_id: "232533265", // este te lo dan despues que te autenticas
-        topics: ["questions"], // Aquí puedes agregar más "topics" si necesitas más eventos
+        topic: "questions",  // Puedes agregar más topics si es necesario
         callback_url: CALLBACK_URL,
       },
       {
@@ -22,12 +19,9 @@ async function subscribeToWebhook() {
         },
       }
     );
-    console.log("Suscripción a webhook de meli exitosa:", response.data);
+    console.log("Suscripción a webhook de Mercado Libre exitosa:", response.data);
   } catch (error) {
-    console.error(
-      "Error al suscribirse al webhook de meli:",
-      error.response?.data || error.message
-    );
+    console.error("Error al suscribirse al webhook de Mercado Libre:", error.response?.data || error.message);
   }
 }
 
