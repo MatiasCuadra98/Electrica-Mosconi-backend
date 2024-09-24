b92c85daf714aeb2c3a98398d61ea3d6a333da3e
Matías Cuadra <MatiasCuadra98>
2024-09-24 08:13:05 -0300

    modifico acces token

--- src/routes/mercadoLibre/mercadoLibreAuth.js
+++ src/routes/mercadoLibre/mercadoLibreAuth.js
@@ -3,11 +3,11 @@ const meliAuth = Router();
 require('dotenv').config();
 
 
-const clientId = "5980219025679562";
+const appId = "5980219025679562";
 const redirectUri = "https://electrica-mosconi-server.onrender.com/callback";
 
 meliAuth.get('/auth', (req, res) => {
-    const authUrl = `https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}`;
+    const authUrl = `https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id=${appId}&redirect_uri=${redirectUri}`;
     res.redirect(authUrl);
 });
 
--- src/services/mercadoLibreSubscriptionWebhook.js
+++ src/services/mercadoLibreSubscriptionWebhook.js
@@ -1,22 +1,21 @@
 const axios = require("axios");
-require('dotenv').config();
 
 async function subscribeToMeliWebhook() {
-  const ACCESS_TOKEN = 'APP_USR-5980219025679562-092318-cd2251e9bdd85ffcae52c42fad2948f3-232533265'; // Usar el nuevo token
-  const CALLBACK_URL = 'https://electrica-mosconi-server.onrender.com/webhook/mercadolibre'; // URL del webhook en tu servidor
-  const USER_ID = '232533265'; // El User ID obtenido
-  const MELI_CLIENT_ID = "5980219025679562"
+  const accessToken = 'APP_USR-5980219025679562-092407-82b9690f26e3ff37065cded053f82a19-232533265'; // Usar el nuevo token
+  const webhookUrl = 'https://electrica-mosconi-server.onrender.com/webhook/mercadolibre'; // URL del webhook en tu servidor
+  const userId = '232533265'; // El User ID obtenido
+  const appId = "5980219025679562"
   
   try {
     const response = await axios.post(
-      `https://api.mercadolibre.com/users/${USER_ID}/applications/${MELI_CLIENT_ID}/notifications`, // URL correcta para suscribirte
+      `https://api.mercadolibre.com/users/${userId}/applications/${appId}/notifications`, // URL correcta para suscribirte
       {
-        topic: 'questions', // Escuchando preguntas, puedes agregar otros eventos aquí
-        callback_url: CALLBACK_URL,
+        topic: ['questions', 'messages'], // Escuchando preguntas, puedes agregar otros eventos aquí
+        callback_url: webhookUrl,
       },
       {
         headers: {
-          Authorization: `Bearer ${ACCESS_TOKEN}`,
+          Authorization: `Bearer ${accessToken}`,
         },
       }
     );
