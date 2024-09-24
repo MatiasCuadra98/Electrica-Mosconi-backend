d2b3ffb2ab8d9939c0384992607c5a25672f3c11
Matías Cuadra <MatiasCuadra98>
2024-09-24 08:44:16 -0300

    cambio client id - app id

--- src/routes/mercadoLibre/mercadoLibreAuth.js
+++ src/routes/mercadoLibre/mercadoLibreAuth.js
@@ -3,11 +3,11 @@ const meliAuth = Router();
 require('dotenv').config();
 
 
-const appId = "5980219025679562";
-const redirectUri = "https://electrica-mosconi-server.onrender.com/callback";
+const client_id = "5980219025679562";
+const redirect_uri = "https://electrica-mosconi-server.onrender.com/callback";
 
 meliAuth.get('/auth', (req, res) => {
-    const authUrl = `https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id=${appId}&redirect_uri=${redirectUri}`;
+    const authUrl = `https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}`;
     res.redirect(authUrl);
 });
 
