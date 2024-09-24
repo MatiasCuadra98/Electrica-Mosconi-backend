bd3b49ef990d2c41b05ab142cdc12ae6f6511896
Matías Cuadra <MatiasCuadra98>
2024-09-24 08:15:06 -0300

    cambio topics auna funcion nueva

--- src/services/mercadoLibreSubscriptionWebhook.js
+++ src/services/mercadoLibreSubscriptionWebhook.js
@@ -4,13 +4,14 @@ async function subscribeToMeliWebhook() {
   const accessToken = 'APP_USR-5980219025679562-092407-82b9690f26e3ff37065cded053f82a19-232533265'; // Usar el nuevo token
   const webhookUrl = 'https://electrica-mosconi-server.onrender.com/webhook/mercadolibre'; // URL del webhook en tu servidor
   const userId = '232533265'; // El User ID obtenido
-  const appId = "5980219025679562"
-  
+  const appId = '5980219025679562'; // Tu App ID de Mercado Libre
+
   try {
-    const response = await axios.post(
-      `https://api.mercadolibre.com/users/${userId}/applications/${appId}/notifications`, // URL correcta para suscribirte
+    // Suscripción para 'questions'
+    const responseQuestions = await axios.post(
+      `https://api.mercadolibre.com/users/${userId}/applications/${appId}/notifications`, 
       {
-        topic: ['questions', 'messages'], // Escuchando preguntas, puedes agregar otros eventos aquí
+        topic: 'questions', // Suscripción a preguntas
         callback_url: webhookUrl,
       },
       {
@@ -19,7 +20,23 @@ async function subscribeToMeliWebhook() {
         },
       }
     );
-    console.log("Suscripción a webhook de Mercado Libre exitosa:", response.data);
+    console.log("Suscripción a 'questions' exitosa:", responseQuestions.data);
+
+    // Suscripción para 'messages'
+    const responseMessages = await axios.post(
+      `https://api.mercadolibre.com/users/${userId}/applications/${appId}/notifications`, 
+      {
+        topic: 'messages', // Suscripción a mensajes
+        callback_url: webhookUrl,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    console.log("Suscripción a 'messages' exitosa:", responseMessages.data);
+
   } catch (error) {
     console.error("Error al suscribirse al webhook de Mercado Libre:", error.response?.data || error.message);
   }
