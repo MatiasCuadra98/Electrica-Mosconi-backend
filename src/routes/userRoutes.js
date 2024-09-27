b7a317c7353de760c2ab55028a243899df12fe94
Virginia Montoya <mvirginiamontoya@yahoo.com.ar>
2024-09-27 14:33:21 -0300

    quito login logout

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,9 +4,6 @@ const {getUserByIdHandler} = require("../handlers/User/getUserByIdHandler");
 const {createUserHandler} = require("../handlers/User/createUserHandler");
 const {updateUserHandler} = require("../handlers/User/updateUserHandler");
 const {deleteUserHandler} = require("../handlers/User/deleteUserHandler");
-const {loginAdmiHandler} = require('../handlers/User/loginAdmiHandler')
-const {logoutAdmiHandler} = require('../handlers/User/logoutAdmiHandler');
-
 
 const userRoute = Router();
 
@@ -14,8 +11,6 @@ userRoute.get("/", getAllUsersHandler);
 userRoute.get("/:id", getUserByIdHandler);
 userRoute.post("/create", createUserHandler);
 userRoute.put("/update/:id", updateUserHandler);
-userRoute.put('login/admi/:id', loginAdmiHandler);
-userRoute.put('logout/admi/:id', logoutAdmiHandler);
 userRoute.delete("/delete/:id", deleteUserHandler);
 
 module.exports = userRoute;
