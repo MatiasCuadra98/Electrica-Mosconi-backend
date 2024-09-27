8ee0bd52a0fb644dee283a9a28df912c01b6765e
Virginia Montoya <mvirginiamontoya@yahoo.com.ar>
2024-09-27 12:06:15 -0300

    login logout admi

---
+++ src/controllers/User/loginAdmi.js
@@ -0,0 +1,15 @@
+const { User } = require('../../../db');
+
+const loginAdmi = async (id) => {
+    if(!id) throw new Error('Missing ID')
+    const admi = await User.findByPk(id);
+    if(!admi) {
+        throw new Error (`User with Id ${id} not found`);
+    } else {
+        admi.login = true
+        await admi.save();
+        return(`Welcome ${admi.name}!`)
+    }   
+};
+
+module.exports = {loginAdmi};
