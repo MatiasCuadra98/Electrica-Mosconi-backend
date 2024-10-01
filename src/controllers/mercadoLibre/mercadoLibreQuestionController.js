6b551c7e6e6c00f7172d2931eff82ef011bbb524
Matías Cuadra <MatiasCuadra98>
2024-10-01 13:49:12 -0300

    Agrego itemId a controller y handler para obtener el id que queremos buscar las preguntas

--- src/controllers/mercadoLibre/mercadoLibreQuestionController.js
+++ src/controllers/mercadoLibre/mercadoLibreQuestionController.js
@@ -1,16 +1,19 @@
 const axios = require('axios');
 
 const mercadoLibreQuestionController = {
-    // Obtiene las preguntas de los productos
-    getQuestions: async (accessToken) => {
-        const response = await axios.get('https://api.mercadolibre.com/questions/search', {
-            headers: { Authorization: `Bearer ${accessToken}` },
-            params: {
-                status: 'UNANSWERED' // Por ejemplo, preguntas no respondidas
-            }
-        });
-
-        return response.data;
+    getQuestions: async (accessToken, itemId) => {
+        try {
+            const response = await axios.get('https://api.mercadolibre.com/questions/search', {
+                headers: { Authorization: `Bearer ${accessToken}` },
+                params: {
+                    item: itemId
+                }
+            });
+            return response.data;
+        } catch (error) {
+            console.error('Error al obtener las preguntas:', error);
+            throw error;
+        }
     }
 };
 
