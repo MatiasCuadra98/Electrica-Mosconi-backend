c748a8ca19f4bd7864873bb0bd3c20d4bae8227a
Matías Cuadra <MatiasCuadra98>
2024-10-03 15:54:33 -0300

    agrego guardado en base de datos de tokens y consultas

--- src/controllers/mercadoLibre/mercadoLibreQuestionController.js
+++ src/controllers/mercadoLibre/mercadoLibreQuestionController.js
@@ -1,20 +1,45 @@
-const axios = require('axios');
+const axios = require("axios");
+const { MsgReceived } = require("../../models/MsgReceived");
 
 const mercadoLibreQuestionController = {
-    getQuestions: async (accessToken, itemId) => {
-        try {
-            const response = await axios.get('https://api.mercadolibre.com/questions/search', {
-                headers: { Authorization: `Bearer ${accessToken}` },
-                params: {
-                    item: itemId
-                }
-            });
-            return response.data;
-        } catch (error) {
-            console.error('Error al obtener las preguntas:', error);
-            throw error;
+  getQuestions: async (accessToken, itemId, BusinessId) => {
+    try {
+      const response = await axios.get(
+        "https://api.mercadolibre.com/questions/search",
+        {
+          headers: { Authorization: `Bearer ${accessToken}` },
+          params: { item: itemId, 
+                    BusinessId: "53c2e647-ce26-41f7-915e-aac13b11c92a"
+           },
         }
+      );
+      //agrego variable que guarde las preguntas
+      const questions = response.data.questions;
+
+      //Guardamos las preguntas en la base de datos, iteramos sobre la variable creada antes y guardamos la data
+      for (const question of questions) {
+        await MsgReceived.create({
+            id: question.id, // Usamos el id de la pregunta como id en el modelo
+            chatId: question.from.id, // El id del usuario que hace la pregunta
+            idUser: question.from.id, // Id del usuario que hizo la pregunta
+            text: question.text, // Texto de la pregunta
+            name: question.from.nickname, // Nombre del usuario
+            timestamp: new Date(question.date_created).getTime(), // Convertir la fecha a timestamp
+            phoneNumber: null, // Mercado Libre no proporciona el teléfono directamente asi q lo podemos sacar a esta linea
+            userName: question.from.nickname, // Nombre del usuario
+            Email: null, // Email también no está disponible directamente la podemos sacar a esta linea
+            BusinessId: BusinessId, // El BusinessId que envías desde el handler
+            active: true, // Estado activo del mensaje
+            state: 'No Leidos', // Por defecto "No Leidos"
+            received: true // Indicador de que es un mensaje recibido
+        });
     }
+      return response.data;
+    } catch (error) {
+      console.error("Error al obtener las preguntas:", error);
+      throw error;
+    }
+  },
 };
 
 module.exports = { mercadoLibreQuestionController };
--- src/handlers/MercadoLibre/mercadoLibreAuthHandler.js
+++ src/handlers/MercadoLibre/mercadoLibreAuthHandler.js
@@ -1,42 +1,85 @@
-const { mercadoLibreAuthController } = require('../../controllers/mercadoLibre/mercadoLibreAuthController');
+const {
+  mercadoLibreAuthController,
+} = require("../../controllers/mercadoLibre/mercadoLibreAuthController");
+const { SocialMediaActive } = require("../../models/SocialMediaActive");
 
 const mercadoLibreAuthHandler = async (req, res) => {
-    try {
-        if (req.query.code) {
-            const accessToken = await mercadoLibreAuthController.getAccessToken(req.query.code);
-            console.log("Token de acceso recibido:", accessToken);
-            
-            // Guarda el token en la sesión o base de datos
-            req.session.accessToken = accessToken; // Ejemplo de guardado en sesión
-            return res.json({ accessToken });
-        } else {
-            const authUrl = mercadoLibreAuthController.getAuthUrl();
-            return res.redirect(authUrl);
-        }
-    } catch (error) {
-        console.error("Error al redirigir a Mercado Libre:", error);
-        res.status(500).json({ message: 'Error en la autenticación con Mercado Libre' });
+  try {
+    if (req.query.code) {
+      //const accessToken = await mercadoLibreAuthController.getAccessToken(req.query.code);
+      //console.log("Token de acceso recibido:", accessToken);
+      const { accessToken, refreshToken, authorizationCode } =
+        await mercadoLibreAuthController.getAccessToken(req.query.code);
+      console.log("Tokens de meli recibidos:", {
+        accessToken,
+        refreshToken,
+        authorizationCode,
+      });
+      // Guardamos tokens en la bd
+      await SocialMediaActive.create({
+        dataUser: "+54 9 3487 34-7843", // Asegúrate de reemplazarlo por la data correcta
+        active: true, // Puedes modificar según tu lógica
+        socialMediaId: 5, // Reemplaza por el valor correcto de la red social
+        accessToken: accessToken,
+        refreshToken: refreshToken,
+        authorizationCode: authorizationCode,
+      });
+      return res.json({
+        message: "Tokens guardados correctamente en la base de datos",
+      });
+    } else {
+      const authUrl = mercadoLibreAuthController.getAuthUrl();
+      return res.redirect(authUrl);
     }
+  } catch (error) {
+    console.error("Error al redirigir a Mercado Libre:", error);
+    res
+      .status(500)
+      .json({ message: "Error en la autenticación con Mercado Libre" });
+  }
 };
 
 const mercadoLibreCallbackHandler = async (req, res) => {
-    try {
-        const { code } = req.query;
+  try {
+    const { code } = req.query;
 
-        if (!code) {
-            return res.status(400).json({ message: 'No se proporcionó el código de autorización.' });
-        }
-
-        console.log('Código de autorización recibido:', code);
-        const accessToken = await mercadoLibreAuthController.getAccessToken(code);
-        
-        console.log("Token de acceso recibido:", accessToken);
-        req.session.accessToken = accessToken; // Guardar el token en sesión o base de datos
-        return res.json({ accessToken });
-    } catch (error) {
-        console.error("Error al obtener el token de acceso:", error.response ? error.response.data : error.message);
-        res.status(500).json({ message: 'Error al obtener el token de acceso' });
+    if (!code) {
+      return res
+        .status(400)
+        .json({ message: "No se proporcionó el código de autorización." });
     }
+
+    console.log("Código de autorización recibido:", code);
+    //const accessToken = await mercadoLibreAuthController.getAccessToken(code);
+    const { accessToken, refreshToken, authorizationCode } =
+      await mercadoLibreAuthController.getAccessToken(code);
+
+    //console.log("Token de acceso recibido:", accessToken);
+    console.log("Tokens recibidos:", {
+      accessToken,
+      refreshToken,
+      authorizationCode,
+    });
+    // Guardar los tokens en la base de datos
+    await SocialMediaActive.create({
+      dataUser: "+54 9 3487 34-7843", // Asegúrate de reemplazarlo por la data correcta
+      active: true, // Puedes modificar según tu lógica
+      socialMediaId: 5, // Reemplaza por el valor correcto de la red social
+      accessToken: accessToken,
+      refreshToken: refreshToken,
+      authorizationCode: authorizationCode,
+    });
+
+    return res.json({
+      message: "Tokens guardados correctamente en la base de datos",
+    });
+  } catch (error) {
+    console.error(
+      "Error al obtener el token de acceso:",
+      error.response ? error.response.data : error.message
+    );
+    res.status(500).json({ message: "Error al obtener el token de acceso" });
+  }
 };
 
 module.exports = { mercadoLibreAuthHandler, mercadoLibreCallbackHandler };
