c97a618eb17d0cf581021bcb96481aa3c4458f3b
Matías Cuadra <MatiasCuadra98>
2024-10-07 14:27:42 -0300

    agrego log asi vemos la Respuesta

--- src/controllers/mercadoLibre/mercadoLibreQuestionController.js
+++ src/controllers/mercadoLibre/mercadoLibreQuestionController.js
@@ -27,6 +27,7 @@ const mercadoLibreQuestionController = {
         headers: { Authorization: `Bearer ${accessToken}` },
         params: { item: itemId },
       });
+      console.log("Respuesta de Mercado Libre:", response.data);
 
       const questions = response.data.questions;
 
